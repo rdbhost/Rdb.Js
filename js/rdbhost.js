352a48bb01801caec4c4ba66b965cd44e0e53831
David <dkeeney@rdbhost.com>
2015-01-09 21:47:31 -0700

    added xhr and login tests added xhr and login functionality

--- js/rdbhost.js
+++ js/rdbhost.js
@@ -7,7 +7,7 @@
  *    html5 browsers.
  *
  *
- *  It has one dependency, on Bluebird promise library.
+ *  It has two dependencies, the Bluebird promise library, and the Jvent library
  *
  *
  *    class Connection({connOps})
@@ -75,6 +75,168 @@
         return roleType.substr(0, 1) + numStr.substr(numStr.length - 10, 10);
     }
 
+    /*
+     Return API type for data item.
+
+     return value is one of STRING, NUMBER, DATE, NONE, DATETIME, TIME
+     */
+    function apiType(d) {
+
+        switch (typeof d) {
+
+            case 'number':
+                return 'NUMBER';
+            case 'object':
+                if ($.type(d) == 'date')
+                    return 'DATETIME';
+                else if ($.type(d) == 'null')
+                    return 'NONE';
+                else
+                    return 'STRING';
+            case 'undefined':
+                return 'NONE';
+
+            default:
+                return 'STRING'
+        }
+    }
+
+    function handleResponseData(msg, resolve, reject) {
+
+        if ( msg['status'][0] === 'error' ) {
+
+            // check for whitelist errors
+            if (msg['error'][0] === '') {
+
+                var p2 = getLoginEmailAndPassword()
+                        .then(function(EmlNPw) {
+                            var eml = emlNPw[0], pw = emlNPw[1];
+                        // todo - login here, get authcode, continue chain
+                        });
+                resolve(p2)
+            }
+            else {
+                reject(msg['error']);
+                // todo - add code to emit on errorHandler also
+            }
+        }
+        // 'incomplete' and 'complete' result-sets are handled as success
+        //  perhaps 'incomplete' should be an error
+        else {
+            resolve(msg);
+        }
+    }
+
+    function xhrPostAsync(url, formData) {
+
+        return new Promise(function (resolve, reject) {
+
+            function onHttpSuccess() {
+                try {
+                    var data = JSON.parse(xhr.responseText);
+                    handleResponseData(data, resolve, reject);
+                }
+                catch(e) {}
+            }
+            function onHttpFail(err) {
+                reject(err);
+            }
+
+            // var xhr = new XMLHttpRequest;
+            var xhr = getRequestObject();
+            xhr.addEventListener("error", onHttpFail);
+            xhr.addEventListener("load", onHttpSuccess);
+            xhr.open("POST", url);
+            xhr.send(formData);
+        });
+    }
+
+    function _login(url, formData) {
+
+        return new Promise(function (resolve, reject) {
+
+            function onHttpSuccess() {
+                try {
+                    var data = JSON.parse(xhr.responseText);
+                    if ( data.status[0] === 'error' )
+                        reject(data.status);
+                    else
+                        resolve(data.roles.s);
+                }
+                catch(e) {}
+            }
+            function onHttpFail(err) {
+                reject(err);
+            }
+
+            // var xhr = new XMLHttpRequest;
+            var xhr = getRequestObject();
+            xhr.addEventListener("error", onHttpFail);
+            xhr.addEventListener("load", onHttpSuccess);
+            xhr.open("POST", url);
+            xhr.send(formData);
+        });
+    }
+
+    // loginFromForm - login, taking data from form
+    //
+    function loginFromForm(url, form) {
+
+        var formData = new FormData(form);
+        return _login(url, formData);
+    }
+
+    // login - login using provided parameters.
+    //
+    function login(url, eml, passwd) {
+        var fD = new FormData();
+        fD.append('email', eml);
+        fD.append('password', passwd);
+        return _login(url, fD);
+    }
+
+    // loadLoginForm - reads login-dialog template file from disk, appends
+    //   it to the DOM, and hides it.
+    //   returns Promise only for sequencing, no content is provided to chain.
+    //
+    function loadLoginForm() {
+
+        var url = 'login_form.tpl.html';
+
+        return new Promise(function (resolve, reject) {
+
+            function onHttpSuccess() {
+                var div = document.createElement('div');
+                div.innerHTML = xhr.responseText;
+                document.body.appendChild(div);
+                resolve(true);
+            }
+            function onHttpFail(err) {
+                reject(err);
+            }
+
+            // var xhr = new XMLHttpRequest;
+            var xhr = getRequestObject();
+            xhr.addEventListener("error", onHttpFail);
+            xhr.addEventListener("load", onHttpSuccess);
+            xhr.open("GET", url);
+            xhr.send(null);
+        });
+    }
+
+    // create xmlHttpRequest object
+    //
+    function getRequestObject() {
+        for(var a=0; a<4; a++) {
+            try {                  // try
+                return a               // returning
+                    ? new ActiveXObject([ , "Msxml2", "Msxml3", "Microsoft" ][a] + ".XMLHTTP")
+                    : new XMLHttpRequest
+            }
+            catch(e){}           // ignore each when it fails.
+        }
+    }
+
     // create a connection object
     //
     function Connection(connOpts) {
@@ -83,25 +245,33 @@
             roleType = connOpts.roleType.substr(0, 1),
             host = connOpts.host || 'www.rdbhost.com',
             authcode = connOpts.authcode,
-            url = 'wss://' + host + '/wsdb/' + roleName(accountId, roleType),
-            sendQueue = [],
+            wsurl = 'wss://' + host + '/wsdb/' + roleName(accountId, roleType),
+            xhrurl = 'https://' + host + '/db/' + roleName(accountId, roleType),
+            liurl = 'https://' + host + '/accountlogin/00' + accountId,
             requestHandlers = {},
             requestIdCtr = 0,
             errorHandler = new Jvent(),
             inboundHandler = new Jvent(),
-            undefined, conn;
+            undefined, conn, sendQueue;
 
         // _connect opens websocket connection and installs handlers on it
         //
         function _connect(url) {
 
+            sendQueue = [];
             try {
                 conn = new WebSocket(url);
+                if (authcode) {
+                    var wsInitializer = {'format': 'json-easy', 'authcode': authcode},
+                        wsInit = JSON.stringify(wsInitializer);
+                    sendQueue.push(wsInit);
+                }
             }
             catch (e) {
                 return undefined;
             }
             conn.onmessage = function (event) {
+
                 var msg = JSON.parse(event.data),
                     rId, _r, resolve, reject;
 
@@ -113,14 +283,7 @@
                         resolve = _r[0]; reject = _r[1];
                         delete requestHandlers[rId];
 
-                        if ( msg['status'][0] === 'error' ) {
-                            reject(msg);
-                        }
-                        // 'incomplete' and 'complete' result-sets are handled as success
-                        //  perhaps 'incomplete' should be an error
-                        else {
-                            resolve(msg);
-                        }
+                        handleResponseData(msg, resolve, reject);
                     }
                     else
                         console.log('request-handler not found for '+rId);
@@ -141,14 +304,15 @@
                 _connect(url);
             }
         }
-        _connect(url);
+        _connect(wsurl);
+
 
         function request_ws(opts) {
 
             return new Promise(function(resolve, reject) {
 
                 if ( opts.form )
-                    throw new Error('forms not permitted in request_ws');
+                    return reject('-', 'forms not permitted in request_ws');
 
                 requestIdCtr += 1;
                 opts['request-id'] = requestIdCtr;
@@ -166,13 +330,76 @@
         }
 
 
+        function request_xhr(opts) {
+
+            try {
+
+                var formData = opts.form,
+                    num, argNm, typName, typ;
+
+                // ensure we have a FormData object, capturing form if provided
+                if ( ! formData )
+                    formData = new FormData();
+                else if ( ! formData.append )
+                    formData = new FormData(formData);
+
+                // add provided args list to formData, naming as necessary
+                if ( opts.args ) {
+                    for (var i=0; i<opts.args.length; i+=1) {
+
+                        num = '00'+ i; num = num.substr(num.length-3,3);
+                        argNm = 'arg'+num;
+                        formData.append(argNm, opts.args[i]);
+                        typ = apiType(opts.args[i]);
+                        typName = 'argtype'+num;
+                        formData.append(typName, typ);
+                    }
+                }
+
+                // copy provided namedParams to formData
+                if ( opts.namedParams ) {
+                    for ( var k in opts.namedParams ) {
+                        if ( opts.namedParams.hasOwnProperty(k) ) {
+
+                            argNm = 'arg:'+k;
+                            formData.append(argNm, opts.namedParams[k]);
+                            typ = apiType(opts.namedParams[k]);
+                            typName = 'argtype:'+k;
+                            formData.append(typName, typ);
+                        }
+                    }
+                }
+
+                // copy various other options to formData
+                if ( opts.authcode || connOpts.authcode ) {
+                    formData.append('authcode', opts.authcode || connOpts.authcode);
+                }
+                if ( opts.q ) {
+                    formData.append('q', opts.q);
+                }
+                if ( opts.mode ) {
+                    formData.append('mode', opts.mode);
+                }
+                formData.append('format', opts.format || connOpts.format || 'json-easy');
+            }
+            catch(e) {
+                console.log('error preparing ajax' + e);
+            }
+
+            // make request, return promise
+            return xhrPostAsync(xhrurl, formData);
+        }
+
+
         // return an object bearing properties for connection functionality
         //
         return {
 
             'execute': function (opts) {
-                // todo - add test and request_xhr
-                return request_ws(opts);
+                if ( opts.form )
+                    return request_xhr(opts);
+                else
+                    return request_ws(opts);
             },
 
             // event handlers are just pass-through
@@ -184,8 +411,18 @@
 
             // expose some websocket properties
             //
-            'readyState': function() { return conn.readyState },
-            'close': function() { return conn.close.apply(conn, arguments) }
+            'readyState': function() {
+                return conn.readyState
+            },
+            'close': function() {
+                return conn.close.apply(conn, arguments);
+            },
+
+            // login
+            //
+            'login': function(eml, pw) {
+                return login(liurl, eml, pw);
+            }
         };
     }
 
--- test/v2/myTests.js
+++ test/v2/myTests.js
@@ -5,6 +5,44 @@
 *
 *
 */
+
+
+module('Login Test');
+
+// create connection
+asyncTest('login ', function() {
+
+    var e = RdbhostConnection({'accountId': acct_number, 'roleType': 'r'}),
+        pw = prompt('enter password for '+demo_email);
+    var p = e.login(demo_email, pw);
+    p.then(function(resp) {
+            ok(resp, 'data received');
+            start();
+        })
+        .catch(function(err) {
+            ok(false, 'catch should not be called');
+            start();
+        })
+});
+
+
+// create connection
+asyncTest('login fail', function() {
+
+    var e = RdbhostConnection({'accountId': acct_number, 'roleType': 'r'});
+    var p = e.login(demo_email, 'not-pw');
+    p.then(function(resp) {
+            ok(false, 'data received');
+            start();
+        })
+        .catch(function(err) {
+            ok(err, 'login failed');
+            start();
+        })
+});
+
+
+
 module('Connection Pre-test');
 
 // create connection
@@ -20,6 +58,8 @@ asyncTest('create Connection', function() {
     }, 1000);
 });
 
+/*
+
 // create connection-fail
 asyncTest('create Connection Fail', function() {
 
@@ -32,6 +72,8 @@ asyncTest('create Connection Fail', function() {
         e.close();
     }, 1000)
 });
+
+*/
 
 
 asyncTest('simple send', function() {
@@ -92,19 +134,184 @@ asyncTest('simple send - fail', function() {
 
         var p = e.execute({'q': 'SELCT 3 AS three'});
         ok(p, 'Promise not Null '+ p);
-        p.catch(function(msg) {
-            err = msg['error'];
-            ok(err[0] === '42601', 'errback called ' + err[0] + ' ' + err[1]);
+        p.then(function(resp) {
+                ok(false, 'bad response');
+                e.close();
+                start();
+            })
+            .error(function(err){
+                ok(err[0] === '42601', 'errback called ' + err[0] + ' ' + err[1]);
+                e.close();
+                start();
+            })
+            .catch(function(err) {
+                var a = 1;
+                return err;
+            });
+    }, 1000);
+
+});
+
+
+asyncTest('simple xhr', function() {
+
+    var e = RdbhostConnection({'accountId': acct_number, 'roleType': 'r'});
+    ok(e.readyState() === 0, 'Bad ReadyState ' + e.readyState());
+
+    var fD = new FormData();
+    var p = e.execute({'q': 'SELECT 3 AS three', 'form': fD});
+    ok(p, 'Promise not Null ' + p);
+    p.then(function(resp) {
+            ok(resp['status'][1].toLowerCase() === 'ok', 'bad response status ' + resp['status'][1]);
             e.close();
             start();
-        });
-        p.then(function(resp) {
+        })
+        .error(function(err) {
+            // err = msg['error'];
+            ok(false, 'errback called ' + err[0] + ' ' + err[1]);
+            e.close();
+            start();
+        })
+        .catch(function() {
+            ok(f, 'catch shouldnt be called');
+            start();
+        })
+});
+
+
+asyncTest('simple xhr - fail', function() {
+
+    var e = RdbhostConnection({'accountId': acct_number, 'roleType': 'r'});
+    ok(e.readyState() === 0, 'Bad ReadyState ' + e.readyState());
+
+    var fD = new FormData();
+    var p = e.execute({'q': 'SELCT 4 AS four', 'form': fD});
+    ok(p, 'Promise not Null ' + p);
+    p.then(function(resp) {
             ok(false, 'bad response');
             e.close();
             start();
-        });
-    }, 1000);
+        })
+        .error(function(err) {
+            // err = msg['error'];
+            ok(err[0] === '42601', 'errback called ' + err[0] + ' ' + err[1]);
+            e.close();
+            start();
+        })
+        .catch(function(err) {
+            ok(false, 'catch shouldnt be called');
+            e.close();
+            start();
+        })
+});
 
+
+asyncTest('simple xhr - args', function() {
+
+    var e = RdbhostConnection({'accountId': acct_number, 'roleType': 'r'});
+    ok(e.readyState() === 0, 'Bad ReadyState ' + e.readyState());
+
+    var fD = new FormData();
+    var p = e.execute({'q': 'SELECT %s AS four, %s as fruit', 'form': fD, 'args': [4, 'BANANA']});
+    ok(p, 'Promise not Null ' + p);
+    p.then(function(resp) {
+            ok(resp['status'][1].toLowerCase() === 'ok', 'bad response status ' + resp['status'][1]);
+            ok(resp.row_count[0] > 0, 'data row found');
+            ok(parseInt(resp.records.rows[0]['four'], 10) === 4, 'data is ' + resp.records.rows[0]['four']);
+            ok(resp.records.rows[0]['fruit'] === 'BANANA', 'data is ' + resp.records.rows[0]['fruit']);
+            e.close();
+            start();
+        })
+        .error(function(err) {
+            ok(false, 'errback called ' + err[0] + ' ' + err[1]);
+            e.close();
+            start();
+        })
+        .catch(function() {
+            ok(false, 'catch shouldnt be called');
+            e.close();
+            start();
+        })
+});
+
+
+asyncTest('simple xhr - namedParams', function() {
+
+    var e = RdbhostConnection({'accountId': acct_number, 'roleType': 'r'});
+    ok(e.readyState() === 0, 'Bad ReadyState ' + e.readyState());
+
+    var fD = new FormData();
+    var p = e.execute({'q': 'SELECT %(_four)s AS four, %(_fruit)s as fruit',
+                       'form': fD, 'namedParams': {'_four': 4, '_fruit': 'PEAR'}});
+    ok(p, 'Promise not Null ' + p);
+    p.then(function(resp) {
+            ok(resp['status'][1].toLowerCase() === 'ok', 'bad response status ' + resp['status'][1]);
+            ok(resp.row_count[0] > 0, 'data row found');
+            ok(parseInt(resp.records.rows[0]['four'], 10) === 4, 'data is ' + resp.records.rows[0]['four']);
+            ok(resp.records.rows[0]['fruit'] === 'PEAR', 'data is ' + resp.records.rows[0]['fruit']);
+            e.close();
+            start();
+        })
+        .error(function(err) {
+            ok(false, 'errback called ' + err[0] + ' ' + err[1]);
+            e.close();
+            start();
+        })
+        .catch(function() {
+            ok(false, 'catch shouldnt be called');
+            e.close();
+            start();
+        })
+});
+
+// do SELECT query form way
+var form = "<form id=\"qunit_form\" method='post' enctype=\"multipart/form-data\">"+
+    "<input name=\"arg000\" value=\"99\" />"+
+    "</form>";
+
+module('Form tests', {
+
+    setup: function () {
+        var f = document.getElementById('qunit_form');
+        if (f)
+            document.body.removeChild(f);
+        document.body.insertAdjacentHTML('beforeend', form);
+    },
+
+    teardown: function () {
+
+        var f = document.getElementById('qunit_form');
+        if (f)
+            document.body.removeChild(f);
+    }
+});
+
+
+asyncTest('simple xhr - form', function() {
+
+    var e = RdbhostConnection({'accountId': acct_number, 'roleType': 'r'});
+    ok(e.readyState() === 0, 'Bad ReadyState ' + e.readyState());
+
+    var f = document.getElementById('qunit_form');
+    var p = e.execute({'q': 'SELECT %s AS four', 'form': f});
+    ok(p, 'Promise not Null ' + p);
+    p.then(function(resp) {
+            ok(resp['status'][1].toLowerCase() === 'ok', 'bad response status ' + resp['status'][1]);
+            ok(resp.row_count[0] > 0, 'data row found');
+            ok(parseInt(resp.records.rows[0]['four'], 10) === 99, 'data is ' + resp.records.rows[0]['four']);
+            e.close();
+            start();
+        })
+        .error(function(err) {
+            ok(false, 'errback called ' + err[0] + ' ' + err[1]);
+            e.close();
+            start();
+        })
+        .catch(function() {
+            ok(false, 'catch shouldnt be called');
+            e.close();
+            start();
+        })
 });
 
 
